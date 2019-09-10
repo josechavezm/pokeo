ec978bde93218798e2b9940b81a6a2b3b644ccef
José Chávez <jsergio.chavez@gmail.com>
2019-09-09 23:05:40 -0500

    no compression

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
 app.use(helmet());
 app.use(cors());
-app.use(compress());
+// app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
